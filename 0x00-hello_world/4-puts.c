dace639533e50d4d73f4e07b1eff0460ad55262a
Daniel-safari <danielsafarimwendwa@gmail.com>
2023-02-16 05:10:19 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.n>
+#include <stdio.h>
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
