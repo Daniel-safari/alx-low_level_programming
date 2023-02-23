35338388eae64df1f5272b3f69a9d062b3dc4bf4
Daniel-safari <danielsafarimwendwa@gmail.com>
2023-02-23 04:01:10 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Print_line - draws a straight line according to parameter
+ * @n: the number of lines to draw
+ * Return: empty
+ */
+
+void Print_line(void)
+{
+	int x;
+
+	if (n <= 0; n++)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
