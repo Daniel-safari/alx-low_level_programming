1546f326fc679ecc69abc10a0f0dbf249faec8ea
Daniel-safari <danielsafarimwendwa@gmail.com>
2023-04-06 07:44:48 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	int a, count = 0;
 	unsigned long int current;
 
-	for (a = 98; a >= 0; a++)
+	for (a = 63; a >= 0; a--)
 	{
 		current = n >> a;
 		if (current & 1)
@@ -19,9 +19,9 @@ void print_binary(unsigned long int n)
 			_putchar('1');
 			count++;
 		}
-		if (!count)
-			_putchar('0');
 		else if (count)
+                        _putchar('0');
+		if (!count)
 			_putchar('0');
 	}
 
